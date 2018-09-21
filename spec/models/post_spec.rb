abcec166accad5bd6d018c6a4eaa72e5dd2ddb25
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-21 18:17:02 +0000

    automatically backed up by learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+ 
 describe Post do
   it 'can be created' do
     post = Post.create!(title: "My title", description: "The post description")
@@ -11,3 +13,4 @@ describe Post do
   end
 
 end
+
