b64c7a053559b2479fec7e3f0386928c3c8f88f9
Nate Dietz <nsdietz@gmail.com>
2019-01-23 23:46:25 +0400

    Done.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# RSpec.describe Post, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe Post do
+  it 'can be created' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
+
+  it 'has a summary' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post.post_summary).to eq("My title - The post description")
+  end
+
+end
